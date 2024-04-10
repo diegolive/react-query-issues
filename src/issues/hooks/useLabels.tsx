f73e4ab64230be175b9157b25bb7aefa511afcc8
diegolive <diegolive@gmail.com>
2024-04-10 20:50:22 +0200

    feat: fin sección 3

---
+++ src/issues/hooks/useLabels.tsx
@@ -0,0 +1,47 @@
+import { useQuery } from "@tanstack/react-query";
+import { githubApi } from "../../api/githubApi";
+import { Label } from "../interfaces/label";
+import { sleep } from "../../helpers/sleep";
+
+const getLabels = async ():Promise<Label[]> => {
+  await sleep(2)
+  const { data } = await githubApi.get<Label[]>('/labels');
+  return data
+}
+
+export const useLabels = () => {
+
+  const labelsQuery = useQuery({
+    queryKey: ["labels"],
+    queryFn: getLabels,
+    staleTime: 1000 * 60 * 60,
+    // refetchOnWindowFocus: 'always',
+    // initialData: []
+    placeholderData: [
+      {
+        id: 717031390,
+        node_id: "MDU6TGFiZWw3MTcwMzEzOTA=",
+        url: "https://api.github.com/repos/facebook/react/labels/good%20first%20issue",
+        name: "good first issue",
+        color: "6ce26a",
+        default: true,
+      },
+      {
+        id: 6344006318,
+        node_id: "LA_kwDOAJy2Ks8AAAABeiHarg",
+        url: "https://api.github.com/repos/facebook/react/labels/fb-exported",
+        name: "fb-exported",
+        color: "ededed",
+        default: false,
+      },
+    ],
+  });
+
+  console.log({ dato1: labelsQuery.data?.at(1) } )
+
+  return { 
+    labelsQuery 
+  }
+
+}
+
